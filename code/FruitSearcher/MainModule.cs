351a15c6d12ee22a71570b06194ac6479fee354b
Emil Cardell <emil.cardell@gmail.com>
2014-10-08 23:47:11 +0200

    Added code projects

---
+++ code/FruitSearcher/MainModule.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+
+namespace FruitSearcher
+{
+	public class MainModule : NancyModule
+	{
+		public MainModule()
+		{
+			Get["/"] = _ => "Hello World!";
+		}
+
+	}
+}
