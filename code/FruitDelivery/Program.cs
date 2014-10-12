2a2ed199f8aa779523d9d6227c5e201ff2314426
Emil Cardell <emil.cardell@gmail.com>
2014-10-13 00:13:49 +0200

    Added fruit delivery, added Nlog.

--- code/FruitDelivery/Program.cs
+++ code/FruitDelivery/Program.cs
@@ -2,14 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using NLog;
 
 namespace FruitDelivery
 {
 	class Program
 	{
+		private static Logger logger = LogManager.GetCurrentClassLogger();
+
 		static void Main(string[] args)
 		{
+			var fruits = new List<string> { "Banana", "Kiwi", "Orange", "Apple", "Pear" };
+			var rand = new Random();
+			var client = new ElasticsearchClient(); 
+
+
+			while(true)
+			{
+				var fruit = new FruitDelivery() {
+					Id = Guid.NewGuid().ToString(),
+					Name = fruits[rand.Next(0, fruits.Length)],
+					Quantity = rand.Next(1, 20)
+				}
+
+				var indexResponse = client.Index("fruit","fruit",fruit.Id, fruit);
+
+				Thread.Sleep(10000);
+			}
 		}
 	}
 }
