238626ddd67f300b1b883a29c89717a22ddf5c7f
Emil Cardell <emil.cardell@gmail.com>
2015-05-11 22:24:34 +0200

    Added missing files

---
+++ code/FruitLogging/InfluxDbOutput.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InfluxDB.Net;
+using InfluxDB.Net.Models;
+using LogFlow;
+
+namespace FruitLogging
+{
+	public class InfluxDbOutput : LogProcessor
+	{
+		public override Result Process(Result result)
+		{
+			if(!result.EventTimeStamp.HasValue)
+				return result;
+
+			var t = result.EventTimeStamp.Value - new DateTime(1970, 1, 1);
+			var secondsSinceEpoch = (long)t.TotalMilliseconds;
+
+			var _client = new InfluxDb("http://192.168.33.101:8086", "root", "root");
+			Serie serie = new Serie.Builder("iislog")
+							.Columns("status", "query", "time")
+							.Values(result.Json.Value<int>("sc-status").ToString(), result.Json.Value<string>("cs-uri-query"), secondsSinceEpoch)
+							.Build();
+			Task.Run(() => _client.WriteAsync("graphana", TimeUnit.Milliseconds, serie));
+
+			return result;
+		}
+	}
+}
